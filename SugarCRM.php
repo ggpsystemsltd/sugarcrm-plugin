280effefd9439ba532ea0b6e4a3e77cd1e5f21a3
Murray Crane <murray.crane@ggpsystems.co.uk>
2021-10-22 15:05:20 +0100

    Working plugin commit
    "Beta testing release" - plugin now has functionality enough to replace "hard-coded functions" in bug_update.php and bug_view_inc.php

--- SugarCRM.php
+++ SugarCRM.php
@@ -1,26 +1,141 @@
 <?php
-class SugarCRMPlugin extends MantisPlugin {
-    function config()
-    {
-        return array(
-            'db_hostname' => 'mysql.home.arpa',
-            'db_username' => 'sugarcrm',
-            'db_password' => 'strong_password',
-            'db_dbname' => 'sugarcrm',
-        );
-    }
 
+/***
+ * SugarCRM integration plugin
+ */
+class SugarCRMPlugin extends MantisPlugin {
+    /***
+     * A method that populates the plugin information and minimum requirements.
+     * @return void
+     */
     function register() {
         $this->name = plugin_lang_get( 'title' );
         $this->description = plugin_lang_get( 'description' );
         $this->page = 'config';
-        $this->version = '0.1';
+        $this->version = '1.3.0';
         $this->requires = array(
-            'MantisCore' => '1.3, 2.0',
+            'MantisCore' => '1.3.0',
         );
 
         $this->author = 'Murray Crane';
         $this->contact = 'murray.crane@ggpsystems.co.uk';
-        $this->url = 'https://wmantisbt.org';
+        $this->url = 'https://www.mantisbt.org';
+    }
+
+    /***
+     * Default plugin configuration.
+     * @return array
+     */
+    function config()
+    {
+        return array(
+            'db_hostname' => 'mysql.example.com',
+            'db_username' => 'sugarcrm',
+            'db_password' => 'strong_password',
+            'db_database' => 'sugarcrm',
+            'case_url'    => 'https://sugarcrm.example.com/#Cases/'
+        );
+    }
+
+    /**
+     * Register events for plugin.
+     */
+    function events() {
+        return array(
+            'EVENT_SUGARCRM_CASE_URL' => EVENT_TYPE_OUTPUT,
+            'EVENT_SUGARCRM_CASE_UPDATE' => EVENT_TYPE_EXECUTE,
+        );
+    }
+
+    /**
+     * Register event hooks for plugin.
+     */
+    function hooks() {
+        return array(
+            'EVENT_SUGARCRM_CASE_URL' => 'getCaseUrl',
+            'EVENT_SUGARCRM_CASE_UPDATE' => 'updateCase',
+        );
+    }
+
+    /***
+     * Get SugarCRM Case UUID using Case Number
+     *
+     * @param int @p_event Whatever
+     * @param int $p_chained_param SugarCRM Case Number
+     *
+     * @return string URL to the SugarCRM Case number
+     */
+    function getCaseUrl( $p_event, $p_chained_param ) {
+        if( $p_chained_param != null ) {
+            return '<a href="' . plugin_config_get( 'case_url' ) . self::getCaseUuid( $p_chained_param ) . '">';
+        }
+    }
+
+    /***
+     * Get SugarCRM Case UUID using Case Number
+     *
+     * @param int $p_casenumber SugarCRM Case number to retrieve UUID for
+     *
+     * @return string UUID that equates to the SugarCRM Case number
+     */
+    function getCaseUuid( $p_casenumber = null ) {
+        if( $p_casenumber != null ) {
+            $t_uuid = null;
+
+            $t_errlevel = error_reporting( 0 );
+            $t_mysqli = new mysqli( plugin_config_get( 'db_hostname' ), plugin_config_get( 'db_username' ), plugin_config_get( 'db_password' ), plugin_config_get( 'db_database' ));
+            error_reporting( $t_errlevel );
+
+            if( $t_mysqli->connect_error ) {
+                die( 'Connect Error (' . $t_mysqli->connect_errno . ') ' . $t_mysqli->connect_error );
+            }
+
+            if($t_stmt = $t_mysqli->prepare( "SELECT id FROM cases WHERE case_number=?" )) {
+                $t_stmt->bind_param( "s", $p_casenumber );
+                $t_stmt->execute();
+                $t_stmt->bind_result( $t_uuid );
+                $t_stmt->fetch();
+                $t_stmt->close();
+            }
+
+            $t_mysqli->close();
+            return $t_uuid;
+        }
+
+        return null;
+    }
+
+    /***
+     * Update SugarCRM Case field/value
+     *
+     * @param array $p_params Case number, field to be updated and value to apply
+     *
+     * @return bool  Success/Failure
+     */
+    public function updateCase($p_event, $p_params ) {
+        $t_rtrn = false;
+        $t_casenumber = $p_params[0];
+        $t_field = $p_params[1];
+        $t_value = $p_params[2];
+
+        if( $t_casenumber != null ) {
+            $t_errlevel = error_reporting( 0 );
+            $t_mysqli = new mysqli( plugin_config_get( 'db_hostname' ), plugin_config_get( 'db_username' ), plugin_config_get( 'db_password' ), plugin_config_get( 'db_database' ));
+            error_reporting( $t_errlevel );
+
+            if( $t_mysqli->connect_errno ) {
+                die( 'Connect error (' . $t_mysqli->connect_errno . ') ' . $t_mysqli->connect_error );
+            }
+
+            $t_query = "UPDATE cases SET $t_field=? WHERE case_number=?";
+            $t_stmt = $t_mysqli->stmt_init();
+            $t_stmt->prepare( $t_query );
+            $t_stmt->bind_param( "si", $t_value, $t_casenumber );
+            $t_rtrn = $t_stmt->execute();
+            $t_stmt->close();
+            $t_mysqli->close();
+        }
+
+        return $t_rtrn;
     }
 }
--- pages/config.php
+++ pages/config.php
@@ -1,36 +1,75 @@
 <?php
+auth_reauthenticate( );
+access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+
+html_page_top( lang_get( 'plugin_SugarCRM_title' ) );
+
+print_manage_menu( );
+
 $t_db_hostname = plugin_config_get( 'db_hostname' );
 $t_db_username = plugin_config_get( 'db_username' );
 $t_db_password = plugin_config_get( 'db_password' );
 $t_db_database = plugin_config_get( 'db_database' );
+$t_case_url = plugin_config_get( 'case_url' );
 ?>
 
-<form action="<?php echo plugin_page( 'config_update' )?>" method="post">
-    <?php echo form_security_field( 'plugin_SugarCRM_config_update' ) ?>
-    <label>
-        Database Host:
-        <input name="db_hostname" value="<?php echo string_attribute( $t_db_hostname ) ?>" />
-    </label>
-    <br>
-    <label>
-        Database Username:
-        <input name="db_username" value="<?php echo string_attribute( $t_db_username ) ?>" />
-    </label>
-    <br>
-    <label>
-        Database Password:
-        <input name="db_password" type="password" value="<?php echo string_attribute( $t_db_password ) ?>" />
-    </label>
-    <br>
-    <label>
-        Database Name:
-        <input name="db_database" value="<?php echo string_attribute( $t_db_database ) ?>" />
-    </label>
-    <br>
-    <label>
-        Reset
-        <input type="checkbox" name="reset" />
-    </label>
-    <br>
-    <input type="submit">
-</form>
+<div id="SugarCRM-config-div" class="form-container">
+	<form id="SugarCRM-config-form" action="<?php echo plugin_page( 'config_edit' )?>" method="post">
+		<fieldset>
+			<legend><span><?php echo plugin_lang_get( 'title' ) . ': ' . plugin_lang_get( 'config' )?></span></legend>
+			<?php echo form_security_field( 'plugin_SugarCRM_config_edit' ) ?>
+
+			<div class="field-container">
+				<label><span><?php echo plugin_lang_get( 'db_hostname' )?></label>
+				<span class="small">
+					<input type="text" name="db_hostname" value="<?php echo string_attribute( $t_db_hostname ) ?>" />
+				</span>
+				<span class="label-style"></span>
+			</div>
+
+            <div class="field-container">
+                <label><span><?php echo plugin_lang_get( 'db_username' )?></label>
+                <span class="small">
+					<input type="text" name="db_username" value="<?php echo string_attribute( $t_db_username ) ?>" />
+				</span>
+                <span class="label-style"></span>
+            </div>
+
+            <div class="field-container">
+                <label><span><?php echo plugin_lang_get( 'db_password' )?></label>
+                <span class="small">
+					<input type="text" name="db_password" value="<?php echo string_attribute( $t_db_password ) ?>" />
+				</span>
+                <span class="label-style"></span>
+            </div>
+
+            <div class="field-container">
+                <label><span><?php echo plugin_lang_get( 'db_database' )?></label>
+                <span class="small">
+					<input type="text" name="db_database" value="<?php echo string_attribute( $t_db_database ) ?>" />
+				</span>
+                <span class="label-style"></span>
+            </div>
+
+            <div class="spacer">
+            </div>
+
+            <div class="field-container">
+                <label><span><?php echo plugin_lang_get( 'case_url' )?>
+                <br /><span class="small"><?php echo plugin_lang_get( 'case_url_info' )?></span>
+                </label>
+                <span class="small">
+					<input type="text" name="case_url" value="<?php echo string_attribute( $t_case_url ) ?>" />
+				</span>
+                <span class="label-style"></span>
+            </div>
+
+            <span class="submit-button">
+				<input type="submit" class="button" value="<?php echo lang_get( 'change_configuration' )?>" />
+			</span>
+		</fieldset>
+	</form>
+</div>
+
+<?php
+html_page_bottom();
