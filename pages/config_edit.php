f06d24bec4b9bb29950e16ad045b2a3ee120d0a0
Murray Crane <murray.crane@ggpsystems.co.uk>
2021-10-22 15:02:05 +0100

    Filemode changes
    LICENSE and README don't need to be 775.

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -1,27 +1,31 @@
 <?php
 form_security_validate( 'plugin_SugarCRM_config_update' );
 
+auth_reauthenticate( );
+access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+
 $f_db_hostname = gpc_get_string( 'db_hostname' );
 $f_db_username = gpc_get_string( 'db_username' );
 $f_db_password = gpc_get_string( 'db_password' );
 $f_db_database = gpc_get_string( 'db_database' );
 $f_reset = gpc_get_bool( 'reset', false );
 
-form_security_purge( 'plugin_SugarCRM_config_update' );
-
-if( $f_reset ) {
-    plugin_config_delete( 'db_hostname' );
-    plugin_config_delete( 'db_username' );
-    plugin_config_delete( 'db_password' );
-    plugin_config_delete( 'db_database' );
-} elseif( $f_db_hostname != '' ) {
+if( plugin_config_get( 'db_hostname' ) != $f_db_hostname ) {
     plugin_config_set( 'db_hostname', $f_db_hostname );
-    plugin_config_set( 'db_username', $f_db_username );
-    plugin_config_set( 'db_password', $f_db_password );
-    plugin_config_set( 'db_database', $f_db_database );
-} else {
-    error_parameters( 'db_hostname', string_attribute( $f_db_hostname ));
-    trigger_error( ERROR_CONFIG_OPT_INVALID, ERROR );
 }
+
+if( plugin_config_get( 'db_username' ) != $f_db_username ) {
+    plugin_config_set( 'db_username', $f_db_username );
+}
+
+if( plugin_config_get( 'db_password' ) != $f_db_password ) {
+    plugin_config_set( 'db_password', $f_db_password );
+}
+
+if( plugin_config_get( 'db_database' ) != $f_db_database ) {
+    plugin_config_set( 'db_database', $f_db_database );
+}
+
+form_security_purge( 'plugin_SugarCRM_config_update' );
 
 print_successful_redirect( plugin_page( 'config', true ));
